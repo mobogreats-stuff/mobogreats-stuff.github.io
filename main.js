f742a8e36788c6fe66dfcacd3db483d8ced6777c
ysry <ysrystudio@gmail.com>
2026-07-22 23:20:26 +0100

    Base for projects
    + get projects
    + project template
    / changed css files into multiple for readability

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const memberList = document.getElementById("member-list");
 const memberTemplate = document.getElementById("member-template");
 
 let members = [];
+let projects = [];
 const member_url = `https://api.github.com/orgs/mobogreats-stuff/members`;
-
+const project_url = `https://api.github.com/orgs/mobogreats-stuff/repos`;
 
 // update member list
 function updateMemberList() {
@@ -44,5 +45,23 @@ async function getOrganizationMembers() {
     }
 }
 
+async function getProjects() {
+    // fetch projects
+    const res = await fetch(project_url);
+    const data = await res.json();
+
+    console.log(data);
+
+    // check for response errors
+    if (data.message == undefined) {
+        projects = data;
+        return projects
+    } 
+    else {
+        throw new Error(data.message || 'Failed to fetch organization member count');
+    }
+}
+
 // RUNTIME
 getOrganizationMembers()
+getProjects()
